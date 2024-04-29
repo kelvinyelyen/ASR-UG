ee3f1ef965aef1a447b86fc1c520b5643202b77a
kelvinyelyen <kelvinyelyen@gmail.com>
2024-04-29 15:44:19 +0000

    [update] new directories

--- src/load_datasets.py
+++ src/load_datasets.py
@@ -10,17 +10,17 @@ GREEN = '\033[92m'
 RESET = '\033[0m'
 
 
-# def download_and_extract_dataset(dataset_link, extract_to):
-#     if os.listdir(extract_to):
-#         print(
-#             f"{BOLD}{GREEN}Dataset directory is not empty. Skipping download and extraction.{RESET}")
-#         return
-#     print(f"{BOLD}Downloading datasets...{RESET}")
-#     dataset_zip_file = wget.download(dataset_link)
-#     print(f"{BOLD}Extracting...{RESET}")
-#     with zipfile.ZipFile(dataset_zip_file, 'r') as zip_ref:
-#         zip_ref.extractall(extract_to)
-#     os.remove(dataset_zip_file)
+def download_and_extract_dataset(dataset_link, extract_to):
+    if os.listdir(extract_to):
+        print(
+            f"{BOLD}{GREEN}Dataset directory is not empty. Skipping download and extraction.{RESET}")
+        return
+    print(f"{BOLD}Downloading datasets...{RESET}")
+    dataset_zip_file = wget.download(dataset_link)
+    print(f"{BOLD}Extracting...{RESET}")
+    with zipfile.ZipFile(dataset_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+    os.remove(dataset_zip_file)
 
 
 def load_data(data_dir):
@@ -79,9 +79,9 @@ def load_data_with_tf(data_dir, batch_size=16, validation_split=0.2, output_sequ
     return training_set, validation_set
 
 
-# dataset_link = "http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip"
-# extract_to = "./data"
-data_dir = "./data/Dagaare/Batch1"
+dataset_link = "http://storage.googleapis.com/download.tensorflow.org/data/mini_speech_commands.zip"
+extract_to = "./data"
+data_dir = "./data/mini_speech_commands"
 
 # Download and extract the dataset
 download_and_extract_dataset(dataset_link, extract_to)
